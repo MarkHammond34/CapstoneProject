ab5773e9aac8190c2046c89a9b559f0a49a4d824
Owner <Owner@10.100.209.174>
2018-01-29 11:42:17 -0600

    Listing Table

---
+++ DBscripts/ListingTable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `listing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL DEFAULT 'default',
+  `description` varchar(45) DEFAULT NULL,
+  `category` varchar(45) NOT NULL DEFAULT 'other',
+  `price` double NOT NULL DEFAULT '0',
+  `date_created` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  `image_path` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `userID_fk_idx` (`userID`),
+  CONSTRAINT `userID_fk` FOREIGN KEY (`userID`) REFERENCES `user` (`user_ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
