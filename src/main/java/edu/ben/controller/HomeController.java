222e21c0cd54b6c980a4fb62f20d097bf6d880c9
Jacob <Jacob@10.0.0.67>
2018-02-05 23:17:25 -0600

    Contact Us email

--- src/main/java/edu/ben/controller/HomeController.java
+++ src/main/java/edu/ben/controller/HomeController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,23 +18,28 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HomeController extends BaseController {
 
-    @Autowired
-    ListingService listingService;
+	@Autowired
+	ListingService listingService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView home(HttpServletRequest request) {
-        ModelAndView model = new ModelAndView("index");
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView home(HttpServletRequest request) {
+		ModelAndView model = new ModelAndView("index");
 
-        List<Listing> recent = listingService.getRecentListings();
-        model.addObject("recentListings", recent);
+		List<Listing> recent = listingService.getRecentListings();
+		model.addObject("recentListings", recent);
 
-        List<Listing> endingSoon = listingService.getRecentListings();
-        model.addObject("endingSoonListings", endingSoon);
+		List<Listing> endingSoon = listingService.getRecentListings();
+		model.addObject("endingSoonListings", endingSoon);
 
-        List<Listing> trending = listingService.getListingsByBidCount();
-        model.addObject("trendingListings", trending);
+		List<Listing> trending = listingService.getListingsByBidCount();
+		model.addObject("trendingListings", trending);
 
-        setRequest(request);
-        return model;
-    }
+		setRequest(request);
+		return model;
+	}
+
+	@GetMapping("/contactUs")
+	public String contactUs() {
+		return "contactUs";
+	}
 }
--- src/main/webapp/WEB-INF/view/jspf/footer.jsp
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p>
-        &copy; 2017<a style="color: #ffffff; text-decoration: none;" href="#">
-        U-ListIt</a>, All rights reserved 2018.
-    </p>
-</footer>
