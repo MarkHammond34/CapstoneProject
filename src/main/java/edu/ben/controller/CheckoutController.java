2662113e2dfad64b96ae308ffff1c01263b45ca7
Mark Hammond <markhammond1044@gmail.com>
2018-04-06 15:36:18 -0500

    Merge branch 'master' of http://cs.ben.edu/bitbucket/scm/sp18capmcjss/u-listit
    # Conflicts:
    #	src/main/java/edu/ben/controller/HomeController.java

--- src/main/java/edu/ben/controller/CheckoutController.java
+++ src/main/java/edu/ben/controller/CheckoutController.java
@@ -1,5 +1,8 @@
 package edu.ben.controller;
 
+import edu.ben.model.PickUp;
+import edu.ben.model.Transaction;
+import edu.ben.model.User;
 import edu.ben.service.ListingService;
 import edu.ben.service.PickUpService;
 import edu.ben.service.TransactionService;
@@ -7,6 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @Transactional
@@ -24,66 +31,66 @@ public class CheckoutController extends BaseController {
     @Autowired
     ListingService listingService;
 
-//    @GetMapping("/checkout")
-//    public String checkoutPageGet(HttpServletRequest request, @RequestParam("l") int listingID) {
-//
-//        try {
-//            User user = (User) request.getSession().getAttribute("user");
-//
-//            if (user == null) {
-//                addWarningMessage("Login To Checkout");
-//                setRequest(request);
-//                return "login";
-//            }
-//
-//            Transaction transaction = transactionService.getTransactionsByListingID(listingID);
-//
-//            // Verify transaction was created
-//            if (transaction == null) {
-//                addErrorMessage("Error Loading Transaction");
-//                setRequest(request);
-//                return "redirect:" + request.getHeader("Referer");
-//            }
-//
-//            // Verify user
-//            if (user.getUserID() != transaction.getBuyer().getUserID() && user.getUserID() != transaction.getSeller().getUserID()) {
-//                addWarningMessage("Access Denied");
-//                setRequest(request);
-//                return "redirect:" + request.getHeader("Referer");
-//            }
-//
-//
-//            PickUp pickUp = pickUpService.getPickUpByListingID(listingID);
-//
-//            if (pickUp == null) {
-//                addErrorMessage("Error Loading Pick Up");
-//                setRequest(request);
-//                return "redirect:" + request.getHeader("Referer");
-//            }
-//
-//            if (pickUp.getBuyerAccept() == 0 && user.getUserID() == transaction.getBuyer().getUserID()) {
-//                addWarningMessage("Pick Up Must Be Accepted Before Checkout");
-//                setRequest(request);
-//                return "redirect:" + request.getHeader("Referer");
-//            }
-//
-//            if (pickUp.getBuyerAccept() == 0 && user.getUserID() == transaction.getSeller().getUserID()) {
-//                addWarningMessage("Pick Up Must Be Accepted By The Buyer Before Checkout");
-//                setRequest(request);
-//                return "redirect:" + request.getHeader("Referer");
-//            }
-//
-//            request.setAttribute("title", "Checkout");
-//            request.setAttribute("listing", pickUp.getTransaction().getListingID());
-//            request.setAttribute("pickUp", pickUp);
-//            setRequest(request);
-//            return "checkout/checkout";
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return "redirect:/";
-//    }
+    @GetMapping("/checkout")
+    public String checkoutPageGet(HttpServletRequest request, @RequestParam("l") int listingID) {
+
+        try {
+            User user = (User) request.getSession().getAttribute("user");
+
+            if (user == null) {
+                addWarningMessage("Login To Checkout");
+                setRequest(request);
+                return "login";
+            }
+
+            Transaction transaction = transactionService.getTransactionsByListingID(listingID);
+
+            // Verify transaction was created
+            if (transaction == null) {
+                addErrorMessage("Error Loading Transaction");
+                setRequest(request);
+                return "redirect:" + request.getHeader("Referer");
+            }
+
+            // Verify user
+            if (user.getUserID() != transaction.getBuyer().getUserID() && user.getUserID() != transaction.getSeller().getUserID()) {
+                addWarningMessage("Access Denied");
+                setRequest(request);
+                return "redirect:" + request.getHeader("Referer");
+            }
+
+
+            PickUp pickUp = pickUpService.getPickUpByListingID(listingID);
+
+            if (pickUp == null) {
+                addErrorMessage("Error Loading Pick Up");
+                setRequest(request);
+                return "redirect:" + request.getHeader("Referer");
+            }
+
+            if (pickUp.getBuyerAccept() == 0 && user.getUserID() == transaction.getBuyer().getUserID()) {
+                addWarningMessage("Pick Up Must Be Accepted Before Checkout");
+                setRequest(request);
+                return "redirect:" + request.getHeader("Referer");
+            }
+
+            if (pickUp.getBuyerAccept() == 0 && user.getUserID() == transaction.getSeller().getUserID()) {
+                addWarningMessage("Pick Up Must Be Accepted By The Buyer Before Checkout");
+                setRequest(request);
+                return "redirect:" + request.getHeader("Referer");
+            }
+
+            request.setAttribute("title", "Checkout");
+            request.setAttribute("listing", pickUp.getTransaction().getListingID());
+            request.setAttribute("pickUp", pickUp);
+            setRequest(request);
+            return "checkout/checkout";
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
 
 
 }
